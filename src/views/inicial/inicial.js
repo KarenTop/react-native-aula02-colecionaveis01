bbaab7fee04743d4a3dc5f902791bab874650a22
KarenTop <karen.rodriguesdisc@gmail.com>
2021-11-26 18:14:35 +0000

    opa

---
+++ src/views/inicial/inicial.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Text, View, TouchableOpacity} from 'react-native';
+import estiloinicial from './estiloinicial';
+
+function Inicial(){
+     
+    return (
+        <View style={estiloinicial.container}>
+            <View style={estiloIinicial.borda}>
+
+                <Text style={estiloinicial.texto}>Inicial</Text>
+
+                <TouchableOpacity style={estiloinicial.botaoContainer}>
+                <Text style={estiloinicial.botaoContainer}>Coleção</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style ={estiloinicial.botaoContainer}>
+                    <Text style={estiloinicial.botaoTexto}>Item</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
