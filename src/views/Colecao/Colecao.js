86548c9e130ea2bfbc7de9b5d0ee5df3803d6d31
KarenTop <karen.rodriguesdisc@gmail.com>
2021-12-09 19:50:09 +0000

    opa

---
+++ src/views/Colecao/Colecao.js
@@ -0,0 +1,20 @@
+import react from 'react';
+import {Text, View, TouchableOpacity} from 'react-native';
+import estiloColecao from './estiloColecao';
+
+function Colecao(){
+    return(
+        <View style={estiloColecao.container}>
+            <View stle={estiloColecao.borda}>
+
+                <Text style={estiloColecao.texto}>Coleção</Text>
+
+                <TouchableOpacity> style={estiloColecao.botaoContainer}>
+                    <Text style={estiloColecao.botaotexto}>Voltar</Text>
+                </TouchableOpacity>
+
+            </View>
+        </View>
+    )
+}
+export default Colecao;
