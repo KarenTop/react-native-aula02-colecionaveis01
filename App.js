67513046be5f7341818c822a95595988e6e5e912
KarenTop <karen.rodriguesdisc@gmail.com>
2021-12-10 17:33:30 +0000

    opa

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavegationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Inicial from './src/views/Inicial/Inicial';
+import Colecao from './src/views/Colecao/Colecao';
+import Item from './src/views/Item/Item';
+
+const Stack = createStackNavigator();
+
+export default function App(){
+  return (
+      <NavegationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name= "Inicial" component={Inicial}/>
+          <Stack.Screen name= "Colecao" component={Colecao}/>
+          <Stack.Screen name= "Item" component={Item}/>
+        </Stack.Navigator>
+      </NavegationContainer>
+  );
+}
+
 
 export default function App() {
   return (
